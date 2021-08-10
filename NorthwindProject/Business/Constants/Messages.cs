f680aafb71685ce3f0b89410fe958a13a50144de
Semih POLAT <69937552+spolt@users.noreply.github.com>
2021-08-11 00:58:35 +0300

    Add files via upload

---
+++ NorthwindProject/Business/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static string ProductsListed = "Ürünler listelendi.";
+        public static string MaintenanceTime = "Sistem bakımdadır.";
+        public static string ProductCountOfCategoryError = "İlgili kategoride ürün sayısı geçersizdir.";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün bulunmaktadır.";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
+        public static string AuthorizationDenied = "Yetkiniz bulunmamaktadır.";
+        public static string UserRegistered = "Kullanıcı kaydı gerçekleştirildi.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string SuccessfulLogin = "Giriş başarılı.";
+        public static string PasswordError = "Hatalı parola.";
+        public static string UserAlreadyExists = "Kullanıcı zaten bulunmaktadır.";
+        public static string AccessTokenCreated = "Yetkilendime anahtarı oluşturuldu.";
+    }
+}
---
+++ NorthwindProject/ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //ProductTest();
+            //CategoryTest();
+        }
+
+        private static void ProductTest()
+        {
+            var productManager = new ProductManager(new EfProductDal(), new CategoryManager(new EfCategoryDal()));
+
+            //productManager.GetAll().ForEach(p => Console.WriteLine(p.ProductName));
+            //productManager.GetAllByCategoryId(2).ForEach(p => Console.WriteLine(p.ProductName));
+
+            var result = productManager.GetProductDetail();
+            if (result.Success) result.Data.ForEach(p => Console.WriteLine(p.ProductName + " / " + p.CategoryName));
+            else Console.WriteLine(result.Message);
+        }
+
+        private static void CategoryTest()
+        {
+            var categoryManager = new CategoryManager(new EfCategoryDal());
+            categoryManager.GetAll().Data.ForEach(c => Console.WriteLine(c.CategoryName));
+        }
+    }
+}
